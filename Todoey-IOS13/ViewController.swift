15b827fe863cc279fd9c6e3ffcae5ef48a74192c
Demidovich Daniil <demidovich.daniil@gmail.com>
2022-03-26 00:26:14 +0400

    Initial Commit

---
+++ Todoey-IOS13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todoey-IOS13
+//
+//  Created by Daniil Demidovich on 26.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
